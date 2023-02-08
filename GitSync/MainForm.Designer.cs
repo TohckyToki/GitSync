98c9dbb7614e43d7c7304851efb77a880fcb01ee
YuengFu <yuengfu.v@outlook.com>
2023-02-08 18:19:03 +0800

    Complete first version.

--- GitSync/MainForm.Designer.cs
+++ GitSync/MainForm.Designer.cs
@@ -32,29 +32,57 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderListBox = new System.Windows.Forms.ListBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
             this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // notifyIcon
             // 
             this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
-            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("MainIcon")));
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
             this.notifyIcon.Text = "GitSync";
             this.notifyIcon.Visible = true;
             // 
             // contextMenuStrip
             // 
             this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enableToolStripMenuItem,
+            this.settingToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip";
-            this.contextMenuStrip.Size = new System.Drawing.Size(94, 26);
+            this.contextMenuStrip.Size = new System.Drawing.Size(112, 70);
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.CheckOnClick = true;
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.enableToolStripMenuItem.Text = "Enable";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.settingToolStripMenuItem.Text = "Setting";
+            this.settingToolStripMenuItem.Click += new System.EventHandler(this.SettingToolStripMenuItemClick);
             // 
             // exitToolStripMenuItem
             // 
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
             // 
@@ -62,16 +90,103 @@
             // 
             this.mainTimer.Tick += new System.EventHandler(this.MainTimerTick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Watch Folders (One folder per line)";
+            // 
+            // folderListBox
+            // 
+            this.folderListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folderListBox.FormattingEnabled = true;
+            this.folderListBox.ItemHeight = 15;
+            this.folderListBox.Location = new System.Drawing.Point(12, 27);
+            this.folderListBox.Name = "folderListBox";
+            this.folderListBox.Size = new System.Drawing.Size(438, 124);
+            this.folderListBox.TabIndex = 2;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveButton.Location = new System.Drawing.Point(375, 201);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButtonClick);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(12, 201);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Perform interval (Seconds)";
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Location = new System.Drawing.Point(382, 157);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(31, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButtonClick);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeButton.Location = new System.Drawing.Point(419, 157);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(31, 23);
+            this.removeButton.TabIndex = 7;
+            this.removeButton.Text = "-";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.RemoveButtonClick);
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folderTextBox.Location = new System.Drawing.Point(12, 157);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(364, 23);
+            this.folderTextBox.TabIndex = 8;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(514, 427);
+            this.ClientSize = new System.Drawing.Size(462, 238);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.folderListBox);
+            this.Controls.Add(this.label1);
             this.Name = "MainForm";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.Text = "Settings";
             this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +196,15 @@
         private ContextMenuStrip contextMenuStrip;
         private ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.Timer mainTimer;
+        private ToolStripMenuItem enableToolStripMenuItem;
+        private ToolStripMenuItem settingToolStripMenuItem;
+        private Label label1;
+        private ListBox folderListBox;
+        private Button saveButton;
+        private NumericUpDown numericUpDown;
+        private Label label2;
+        private Button addButton;
+        private Button removeButton;
+        private TextBox folderTextBox;
     }
 }
--- GitSync/MainForm.cs
+++ GitSync/MainForm.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic.Logging;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Diagnostics;
 using System.Text;
 
@@ -5,63 +8,199 @@ namespace GitSync
 {
     public partial class MainForm : Form
     {
-        private string[] watchFolders = { "E:\\_\\2_s_develop" };
-        private string LogFileName = string.Empty;
-        private bool isRunning = false;
-
+        #region Inner enum
         private enum LogKind
         {
             App, Info, Error
         }
+        #endregion
 
+        #region Fields
+        private string[] watchFolders = Array.Empty<string>();
+        private string logFileName = string.Empty;
+        private CancellationTokenSource cancellationTokenSource = new();
+        private List<Task> tasks = new();
+        private bool needClose = false;
+        #endregion
+
+        #region Construct
         public MainForm()
         {
             InitializeComponent();
-            this.Visible = false;
-            this.ShowInTaskbar = false;
-            this.WindowState = FormWindowState.Minimized;
+            ManuallyInitializeComponent();
+            ApplyConfiguration();
         }
 
-        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        private void ManuallyInitializeComponent()
         {
-            while (isRunning)
-            {
-                Task.Delay(2000);
-            }
-            this.Close();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.mainTimer.Enabled = true;
+            this.numericUpDown.DecimalPlaces = 0;
+            this.numericUpDown.Increment = 1;
+            this.numericUpDown.ThousandsSeparator = false;
+            this.numericUpDown.Minimum = 20;
+            this.numericUpDown.Maximum = 43200;
+            this.folderListBox.SelectionMode = SelectionMode.One;
+
+            this.Load += MainFormLoad;
+            this.FormClosing += MainFormFormClosing;
+            this.enableToolStripMenuItem.CheckedChanged += EnableToolStripMenuItemCheckedChanged;
+            this.notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
         }
 
-        private void MainFormLoad(object sender, EventArgs e)
+        private void ApplyConfiguration()
+        {
+            this.numericUpDown.Value = Properties.Settings.Default.Interval;
+            this.folderListBox.Items.Clear();
+            this.folderListBox.Items.AddRange(Properties.Settings.Default.Folders.Cast<string>().ToArray());
+        }
+        #endregion
+
+        #region Events
+        private void MainFormLoad(object? sender, EventArgs e)
         {
             this.UpdateLogFile();
             this.LogText($"Application is running.", LogKind.App);
 
-            this.mainTimer.Enabled = true;
-            this.mainTimer.Start();
+            this.mainTimer.Interval = Convert.ToInt32(this.numericUpDown.Value * 1000);
+            this.watchFolders = this.folderListBox.Items.Cast<string>().ToArray();
+
+            this.enableToolStripMenuItem.Checked= true;
+        }
+
+        private void MainFormFormClosing(object? sender, FormClosingEventArgs e)
+        {
+            if (!this.needClose)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        private void NotifyIcon_DoubleClick(object? sender, EventArgs e)
+        {
+            this.settingToolStripMenuItem.PerformClick();
+        }
+
+        private void EnableToolStripMenuItemCheckedChanged(object? sender, EventArgs e)
+        {
+            if (enableToolStripMenuItem.Checked)
+            {
+                this.mainTimer.Start();
+                this.WatchAllGitRepository();
+            }
+            else
+            {
+                this.mainTimer.Stop();
+                this.cancellationTokenSource.Cancel();
+                this.cancellationTokenSource = new CancellationTokenSource();
+                this.tasks.Clear();
+            }
+        }
+
+        private void SettingToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            this.ApplyConfiguration();
+            this.ShowInTaskbar = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+        }
+
+        private void AddButtonClick(object sender, EventArgs e)
+        {
+            var text = this.folderTextBox.Text;
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                MessageBox.Show("​The folder path should not be empty.");
+                return;
+            }
+            if (!Directory.Exists(text))
+            {
+                MessageBox.Show("​The folder path does not exist.");
+                return;
+            }
+            if (!this.folderListBox.Items.Contains(text))
+            {
+                MessageBox.Show("The folder path has already been added.");
+                return;
+            }
+            this.folderListBox.Items.Add(text);
+            this.folderTextBox.Clear();
+        }
+
+        private void RemoveButtonClick(object sender, EventArgs e)
+        {
+            if (this.folderListBox.SelectedItem is null)
+            {
+                MessageBox.Show("There should be one item that has been selected at least.");
+                return;
+            }
+            this.folderListBox.Items.Remove(this.folderListBox.SelectedItem);
+        }
+
+        private void SaveButtonClick(object sender, EventArgs e)
+        {
+
+            if (this.folderListBox.Items.Count < 1)
+            {
+                MessageBox.Show("");
+                return;
+            }
+
+            this.mainTimer.Stop();
+            this.cancellationTokenSource.Cancel();
+            this.cancellationTokenSource = new CancellationTokenSource();
+            this.tasks.Clear();
+
+            this.mainTimer.Interval = Convert.ToInt32(this.numericUpDown.Value * 1000);
+            this.watchFolders = this.folderListBox.Items.Cast<string>().ToArray();
+            Properties.Settings.Default.Interval = this.numericUpDown.Value;
+            var sc = new StringCollection();
+            sc.AddRange(this.watchFolders);
+            Properties.Settings.Default.Folders = sc;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show("The new setting has been applied.");
+
+            if (enableToolStripMenuItem.Checked)
+            {
+                this.mainTimer.Start();
+                this.WatchAllGitRepository();
+            }
+        }
+
+        private void ExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+            this.needClose = true;
+            cancellationTokenSource.Cancel();
+            this.Close();
         }
 
         private void MainTimerTick(object sender, EventArgs e)
         {
-            isRunning = true;
-            this.UpdateLogFile();
-
-            foreach (string folder in watchFolders)
-            {
-                this.WatchGitStatus(folder);
-            }
-            this.mainTimer.Interval = 300000;
-            isRunning = false;
+            WatchAllGitRepository();
         }
+        #endregion
 
         private void UpdateLogFile()
         {
-            var logFileName = $"{DateTime.Now.ToString("yyyy-MM-dd-HH")}.log";
-            var fullName = Path.Combine(Application.StartupPath, logFileName);
+            var logFolder = Path.Combine(Application.StartupPath, "logs");
+            if (!File.Exists(logFolder))
+            {
+                Directory.CreateDirectory(logFolder);
+            }
+
+            var logFileName = $"{DateTime.Now.ToString("yyyy-MM-dd")}.log";
+            var fullName = Path.Combine(logFolder, logFileName);
             if (!File.Exists(fullName))
             {
                 File.Create(fullName).Close();
             }
-            this.LogFileName = fullName;
+            this.logFileName = fullName;
         }
 
         private void LogText(string? text, LogKind kind)
@@ -74,7 +213,33 @@ namespace GitSync
                     text,
                     Environment.NewLine
                 };
-                File.AppendAllLines(this.LogFileName, msgs, UTF8Encoding.UTF8);
+                File.AppendAllLines(this.logFileName, msgs, UTF8Encoding.UTF8);
+            }
+        }
+
+        private void WatchAllGitRepository()
+        {
+            this.UpdateLogFile();
+
+            if (this.tasks.Any())
+            {
+                this.cancellationTokenSource.Cancel();
+                this.cancellationTokenSource = new CancellationTokenSource();
+                this.tasks.Clear();
+            }
+
+            foreach (string folder in watchFolders)
+            {
+                var task = new Task(() =>
+                {
+                    this.WatchGitStatus(folder);
+                }, cancellationTokenSource.Token);
+                tasks.Add(task);
+                task.ContinueWith(_ =>
+                {
+                    tasks.Remove(task);
+                });
+                task.Start();
             }
         }
 
